3948761e81e6a22c23c95d4273fbd9745a3531e8
1vnazarov <nazaroffvana.boeing@yandex.ru>
2024-03-19 13:37:38 +0300

    Доделал

--- app/src/main/java/com/example/form_android/MainActivity.kt
+++ app/src/main/java/com/example/form_android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.form_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.form_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also {setContentView(it.root)}
-        
+        binding.radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            if (checkedId == binding.radioButton.id) {
+                binding.editTextNumberDecimal1.visibility = View.VISIBLE
+            } else {
+                binding.editTextNumberDecimal1.visibility = View.GONE
+            }
+        }
     }
 }
